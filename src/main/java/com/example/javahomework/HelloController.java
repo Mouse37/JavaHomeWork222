c0d808644e7e630070a8c5903de34afd312b470f
Mouse37 <92178358+Mouse37@users.noreply.github.com>
2021-10-11 12:48:40 +0300

    Initial commit

---
+++ src/main/java/com/example/javahomework/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.javahomework;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
