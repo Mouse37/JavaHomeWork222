8415624eb8a6945679a51dc78e99aa9160417f5e
Mouse37 <92178358+Mouse37@users.noreply.github.com>
2021-10-11 20:17:04 +0300

    2
    2

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javahomework {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javahomework to javafx.fxml;
-    exports com.example.javahomework;
-}
